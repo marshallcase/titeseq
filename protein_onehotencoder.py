f9dabda18aa2e69c618e48179dd0e775fe80714c
taffyrooster <marshall.acecase@gmail.com>
2022-12-23 14:28:36 -0500

    automatic extraction of varied sites

--- protein_onehotencoder.py
+++ protein_onehotencoder.py
@@ -92,3 +92,70 @@ class ProteinOneHotEncoder(OneHotEncoder):
         # turn the list of chars back into a string
         X = ["".join(i) for i in X]
         return X
+    
+    @staticmethod
+    def get_varying_sites(X,threshold=1,min_count=1):
+        '''
+        get_varying_sites: given a list of aligned protein sequences, find the positions where the protein has been modified in the library mutations and return it as a list of indices
+        
+        Parameters
+        ----------
+        X: protein sequence
+        threshold: minimum number of mutants needed to include a site as varying, default 1
+        min_count: number of appearances necessary to include
+        
+        Outputs
+        -------
+        varying_sites: list of indices
+        
+        '''
+        # get positions that have more than (threshold) unique mutations at a given position
+        differing_positions_threshold = [i for i in range(len(X[0])) if len(set([s[i] for s in X])) >= threshold]
+        
+        #invert min_count
+        min_count = len(X) - min_count
+        
+        #get positions that have more than (num_count) appearances
+        differing_positions_min = [i for i in range(len(X[0])) if len([s[i] for s in X if s[i] == X[0][i]]) < min_count]
+        return np.intersect1d(differing_positions_threshold,differing_positions_min)
+    
+    @staticmethod
+    def transform_varying_sites(X,threshold=1,min_count=1):
+        '''
+        get_varying_sites: given a list of aligned protein sequences, find the positions where the protein has been modified in the library and return the protein sequences that vary only
+        
+        Parameters
+        ----------
+        X: protein sequence
+        
+        Outputs
+        -------
+        X_modified: protein sequence with only mutated positions (for subsequent one hot encoding)
+        
+        '''
+        #get varied sites
+        varied_sites = ProteinOneHotEncoder.get_varying_sites(X,threshold,min_count)
+        
+        X_modified = [getCharsFromString(i,varied_sites) for i in X]
+        
+        return X_modified
+        
+        
+def getCharsFromString(string,indices):
+    '''
+    getCharsFromString: given a sequence, return characters in string according to indices
+    Parameters
+    ----------
+    string: string
+    indices: list of indices
+    Outputs
+    -------
+    output: characters in string according to indices
+    '''
+    
+    try:
+        output = [string[i] for i in indices]
+    except IndexError: 
+        print ('string index out of range')
+        output = []
+    return ''.join(output)
